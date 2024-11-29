bb91ffc72c004ac9a50412d7071429297d5047c0
Eric Kaufmann <eric.kaufmann291@gmail.com>
2024-11-29 15:33:21 +0100

    add rotation to runner scripts

--- run_pointnet.sh
+++ run_pointnet.sh
@@ -14,12 +14,14 @@ source $PROJECT_DIR/.venv/bin/activate
 # Use LR and TRANSFORM environment variables, default if not set
 LR=${LR:-0.1}
 TRANSFORM=${TRANSFORM:-'unit_cube'}
+ROTATION=${ROTATION:-'no_rot'}
 
 python -W ignore $PROJECT_DIR/train_pointnet.py \
   --job_id $SLURM_JOB_ID \
   --num_epochs 100 \
   --lr $LR \
   --transform $TRANSFORM \
-  --num_workers 2
+  --num_workers 2 \
+  --rot $ROTATION
 
 # ['unit_cube', 'zero_center', 'grid64', 'grid128', 'relative_input']
--- run_single_coord_linear_encoder.sh
+++ run_single_coord_linear_encoder.sh
@@ -13,13 +13,15 @@ source $PROJECT_DIR/.venv/bin/activate
 
 # Use LR and TRANSFORM environment variables, default if not set
 LR=${LR:-0.1}
-TRANSFORM=${TRANSFORM:-'grid64'}
+TRANSFORM=${TRANSFORM:-'unit_cube'}
+ROTATION=${ROTATION:-'no_rot'}
 
 python -W ignore $PROJECT_DIR/train_single_coord_linear_encoder.py \
   --job_id $SLURM_JOB_ID \
   --num_epochs 30 \
   --lr $LR \
   --transform $TRANSFORM \
-  --num_workers 2
+  --num_workers 2 \
+  --rot $ROTATION
 
 # ['unit_cube', 'zero_center', 'grid64', 'grid128', 'relative_input']0
